b60640a22f03512cb2d9ad2d07cb78b6c6ae7393
djuscha <ASDjuscha@t-online.de>
2017-09-19 20:02:44 +0200

    Create language_DE.h

---
+++ src/language_DE.h
@@ -0,0 +1,46 @@
+
+/**The MIT License (MIT)
+Copyright (c) 2015 by Daniel Eichhorn
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+See more at http://blog.squix.ch
+Edit by Lukas Luc3as Porubcan 2017
+*/
+
+// This file contain translation of all strings displayed
+// call Translit(FPSTR (please_calibrate))
+const char str_connecting_to_wifi[] PROGMEM = "Verbinden mit WLAN";
+const char str_please_calibrate[] PROGMEM = "Bitte kalibriere\Touch-Bildschirm mit\Berührungspunkt";
+const char str_going_to_sleep[] PROGMEM = "Gehe in den Schlafmodus!";
+const char str_updating_time[] PROGMEM = "Aktualisierungszeit ...";
+const char str_updating_conditions[] PROGMEM = "Aktualisierungsbedingungen ...";
+const char str_updating_forecasts[] PROGMEM = "Prognosenaktualisierung.";
+const char str_updating_astronomy[] PROGMEM = "Aktualisierung Astronomie";
+const char str_sun[] PROGMEM = "Sonne";
+const char str_moon[] PROGMEM = "Mond";
+const char str_current_conditions[] PROGMEM = "Aktuelle Bedingungen";
+const char str_temperature[] PROGMEM = "Temperatur:";
+const char str_feels_like[] PROGMEM = "Gefüllt:";
+const char str_dew_point[] PROGMEM = "Taupunkt:";
+const char str_wind_speed[] PROGMEM = "Wind Stärke:";
+const char str_wind_dir[] PROGMEM = "Windrichtung:";
+const char str_humidity[] PROGMEM = "Luftfeuchtigkeit:";
+const char str_pressure[] PROGMEM = "Druck:";
+const char str_precipitation[] PROGMEM = "Niederschläge:";
+const char str_uv[] PROGMEM = "UV:";
+const char str_description[] PROGMEM = "Beschreibung:";
+const char str_forecasts[] PROGMEM = "Prognosen";
+const char blog_address[] PROGMEM = "https://blog.squix.ch";
